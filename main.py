dcf4fa1eb4070afc9cd3a58156d9f5d8a6daa24e
Kacper Zdrojewski <zdrojewskikacper.53@gmail.com>
2025-09-01 14:55:00 +0200

    Added grid and pygame window

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
+import pygame, sys
+from grid import Grid
 
-def main():
-    print("Welcome to Warehouse Robot Simulation")
+pygame.init()
 
-main()
+screen = pygame.display.set_mode((600, 600))
+pygame.display.set_caption("Warehouse Robot Simulation")
+
+clock = pygame.time.Clock()
+
+game_grid = Grid()
+game_grid.print_grid()
+
+running = True
+while running:
+    pygame.draw.rect(screen, (0, 255, 0), pygame.Rect(0, 0, 60, 60))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    pygame.display.update()
+    clock.tick(60)
+
+pygame.quit()
+sys.exit()
